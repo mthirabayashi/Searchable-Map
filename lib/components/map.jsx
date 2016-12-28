638e4e1817343a3235a789d929e9bb921e50ae4f
Michael Hirabayashi <mthirabayashi@gmail.com>
2016-12-28 14:18:47 -0800

    update default location

--- lib/components/map.jsx
+++ lib/components/map.jsx
@@ -8,9 +8,9 @@ class Map extends React.Component {
   }
   componentDidMount() {
     console.log('map mounted');
-    const uluru = {lat: -25.363, lng: 131.044};
+    const uluru = {lat: 37.773972, lng: -122.431297};
     const map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+      zoom: 10,
       center: uluru
     });
     var marker = new google.maps.Marker({
@@ -24,5 +24,17 @@ class Map extends React.Component {
       <div id='map'>This is the map component</div>
     );
   }
+
+  // render() {
+  //   console.log(this.state.loaded);
+  //
+  //   if (!this.state.loaded) {
+  //     console.log('loading map...');
+  //     return <div id='map'>Loading...</div>;
+  //   }
+  //   return (
+  //     <div id='map'>Map will go here</div>
+  //   );
+  // }
 }
 export default Map;
